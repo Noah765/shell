200f0447a566325e288aac16997e22c1f82af342
Noah765 <noah.landgraf@gmx.de>
2026-03-08 17:39:58 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.treefmt = {
+    url = "github:numtide/treefmt-nix";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    nixpkgs,
+    treefmt,
+    ...
+  }: let
+    eachSystem = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (x: f nixpkgs.legacyPackages.${x});
+
+    formatter = pkgs:
+      (treefmt.lib.evalModule pkgs {
+        programs.alejandra.enable = true;
+        programs.rustfmt.enable = true;
+      }).config.build.wrapper;
+  in {
+    packages = eachSystem (pkgs: rec {
+      default = shell;
+
+      shell = pkgs.rustPlatform.buildRustPackage {
+        name = "shell";
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+      };
+    });
+
+    devShells = eachSystem (pkgs: {default = pkgs.mkShell {packages = [pkgs.cargo (formatter pkgs)];};});
+
+    formatter = eachSystem formatter;
+  };
+}
