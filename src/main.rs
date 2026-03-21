d2d16d9d736503df6dc8b1692583326845ed40d2
Noah765 <noah.landgraf@gmx.de>
2026-03-21 17:44:57 +0100

    main: use everforest theme

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use iced::{Theme, color, theme::Palette};
+
 use crate::shell::Shell;
 
 mod background;
@@ -7,5 +9,16 @@ fn main() -> iced::Result {
     iced::daemon(Shell::new, Shell::update, Shell::view)
         .title("shell")
         .subscription(Shell::subscription)
+        .theme(Theme::custom(
+            "Everforest",
+            Palette {
+                background: color!(0x2d353b),
+                text: color!(0xd3c6aa),
+                primary: color!(0x7fbbb3),
+                success: color!(0xa7c080),
+                warning: color!(0xdbbc7f),
+                danger: color!(0xe67e80),
+            },
+        ))
         .run()
 }
